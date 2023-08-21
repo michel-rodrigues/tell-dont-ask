c259f8f30ecd6348096ddc3a3616e6bf41c4d261
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2023-08-20 21:22:17 -0300

    keep things simple

--- mixins.py
+++
@@ -1,4 +0,0 @@
-class RepresentationMixin:
-    def __repr__(self) -> str:
-        attributes = ", ".join(f"{key}={value}" for key, value in vars(self).items())
-        return f"{self.__class__.__name__}({attributes})"
--- watering_service/models.py
+++ watering_service/models.py
@@ -1,8 +1,6 @@
 from enum import StrEnum
 from typing import Sequence
 
-from mixins import RepresentationMixin
-
 
 class PlantType(StrEnum):
     VEGETABLE = "vegetable"
@@ -10,18 +8,18 @@ class PlantType(StrEnum):
     TREE = "tree"
 
 
-class Plant(RepresentationMixin):
+class Plant:
     def __init__(self, type: PlantType, soil_moisture_percentage: int, watering_count: int):
         self.type = type
         self.soil_moisture_percentage = soil_moisture_percentage
         self.watering_count = watering_count
 
 
-class RaisedBed(RepresentationMixin):
+class RaisedBed:
     def __init__(self, plants: Sequence[Plant]):
         self.plants = plants
 
 
-class WaterValve(RepresentationMixin):
+class WaterValve:
     def __init__(self, open: bool):
         self.open = open
