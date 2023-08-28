908da676d8b771d1555e361e87017d756771c745
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2023-08-28 20:08:15 -0300

    tell, don't ask!

--- watering_service/service.py
+++ watering_service/service.py
@@ -1,7 +1,6 @@
 import collections
-import time
+from watering_service.models import WateringResult
 
-from watering_service.models import PlantType, RaisedBed, WaterValve
 from watering_service.repositories import RaisedBedRepository, WaterValveRepository
 
 
@@ -17,38 +16,11 @@ class Watering:
         self._raised_bed_repository = raised_bed_repository
         self._water_valve_repository = water_valve_repository
 
-    def perform(self, command: WateringCommand) -> bool:
+    def perform(self, command: WateringCommand) -> WateringResult:
         raised_bed = self._raised_bed_repository.get_bed(command.bed_id)
         water_valve = self._water_valve_repository.get_water_valve()
-        successfully_watered = self._water_bed(raised_bed, water_valve)
-        if successfully_watered:
+        result = raised_bed.water_with(water_valve)
+        if result.successful:
             self._water_valve_repository.save(water_valve)
             self._raised_bed_repository.save(raised_bed)
-        return successfully_watered
-
-    def _wating_water_to_flow(self):
-        time.sleep(0.1)
-
-    def _water_bed(self, raised_bed: RaisedBed, water_valve: WaterValve) -> bool:
-        for plant in raised_bed.plants:
-            needs_watering = False
-            match plant.type:
-                case PlantType.VEGETABLE:
-                    needs_watering = plant.soil_moisture_percentage < 60
-                case PlantType.FLOWER:
-                    needs_watering = plant.soil_moisture_percentage < 50
-                case PlantType.TREE:
-                    needs_watering = plant.soil_moisture_percentage < 45
-
-            if needs_watering:
-                try:
-                    water_valve.open = True
-                    plant.soil_moisture_percentage = plant.soil_moisture_percentage + 20
-                    plant.watering_count = plant.watering_count + 1
-                    self._wating_water_to_flow()
-                except InterruptedException:
-                    return False
-                finally:
-                    water_valve.open = False
-
-        return True
+        return result
